751fb5811778f8edb4b4370faf7839648c901a4a
Prajjwal <prajjwaldimri@hotmail.com>
2019-03-08 12:16:41 +0530

    Added consola for console outputs

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const consola = require('consola');
 const { ApolloServer, gql } = require('apollo-server-express');
 
 const typeDefs = gql`
@@ -17,9 +18,9 @@ const resolvers = {
 const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
-server.applyMiddleware({ app });
 app.use(cors());
+server.applyMiddleware({ app });
 
 app.listen(3000, () => {
-  console.log(`🚀 Server ready at http://localhost:3000${server.graphqlPath}`);
+  consola.success(`🚀 Server ready at http://localhost:3000${server.graphqlPath}`);
 });
