1b92403d6d1cf4c5ca89188cfff1e5816c98afc7
Gaurav <gauravuniyal1998@gmail.com>
2019-03-10 16:47:12 +0530

    added username check

--- server/src/graphql/resolvers/user/mutation.js
+++ server/src/graphql/resolvers/user/mutation.js
@@ -1,9 +1,12 @@
 
 
 import validator from 'validator';
+import { UserInputError } from 'apollo-server-express';
 
-
-const createAccount = () => {
+const createAccount = (parent, args) => {
+  if (!validator.isAlphanumeric(args.user.username)) {
+    throw new UserInputError('username can only be alphanumeric');
+  }
   throw new Error('Method not implemented!');
 };
 
