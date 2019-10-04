04a234255be687bacd7c170d4e946e06691359aa
Sunil Kandpal <sskandpal.uk@gmail.com>
2019-10-04 13:02:59 +0530

    Added post schema

---
+++ server/src/models/post.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from 'mongoose';
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    kudos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    boo: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment',
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model('Post', postSchema);
