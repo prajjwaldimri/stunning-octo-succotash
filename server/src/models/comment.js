37e6804238772409b375e5bba0ae60aebcd49776
Sunil Kandpal <sskandpal.uk@gmail.com>
2019-10-10 16:50:57 +0530

    Fixed spelling mistakes in comment.js

--- server/src/models/comment.js
+++ server/src/models/comment.js
@@ -1,32 +1,33 @@
-import mongoose, {Schema}  from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
 const commentSchema = new Schema(
-    {
-        author: {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        },
-        post: {
-          type: Schema.Types.ObjectId,
-          ref: 'Post'
-        },
-        body: {
-            type: Stirng,
-            required: true
-        },
-        kudos: [
-            {
-              type: Schema.Types.ObjectId,
-              ref: 'User',
-            },
-          ],
-        booo: [
-            {
-              type: Schema.Types.ObjectId,
-              ref: 'User',
-            },
-          ], 
+  {
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
     },
-    {timestamps: true},
+    post: {
+      type: Schema.Types.ObjectId,
+      ref: 'Post',
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    kudos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    booo: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  { timestamps: true },
 );
+
 module.exports = mongoose.model('Comment', commentSchema);
