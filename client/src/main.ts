48bf547c8276c551349cba9a1840c748622fd460
kaisharma04 <krishanu.sharma619@gmail.com>
2019-11-08 15:55:07 +0530

    setting up client server using vue cli

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import App from "./App.vue";
+import App from "./app/App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
@@ -11,3 +11,16 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
+
+router.beforeEach((to, from, next) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ["/login", "/register"];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem("user");
+
+  if (authRequired && !loggedIn) {
+    return next("/login");
+  }
+
+  next();
+});
--- client/src/router.ts
+++ client/src/router.ts
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+
+import HomePage from "./home/Home.vue";
+import LoginPage from "./login/LoginPage.vue";
+import RegisterPage from "./register/RegisterPage.vue";
 
 Vue.use(Router);
 
@@ -9,18 +12,22 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: "/home",
+      name: "Home",
+      component: HomePage
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
+      path: "/login",
+      name: "LoginPage",
+      component:LoginPage
+    },
+    {
+      path: "/register",
+      name: "RegisterPage",
+      component:RegisterPage
+    },
+
+    // otherwise redirect to home
+    { path: "*", redirect: "/" }
   ]
 });
