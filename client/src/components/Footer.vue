c397f06b76ec87c9ae01d4f5f0fe83844eafd136
gauravuniyal1998 <gauravuniyal1998@gmail.com>
2019-11-21 12:44:00 +0530

    Fixed footer position

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,16 +1,28 @@
 <template>
-        <div class="row">
+
+        <div class="footer">
             <div class="col-xs-12">
                 <footer>
-                    <p>This is the footer</p>
+                    <p><b>This is the footer</b></p>
                 </footer>
             </div>
         </div>
-</template>
 
+
+</template>
+<style>
+.footer
+{
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: #049892;
+    color: #000000;
+    text-align: center;
+
+}
+</style>
 <script>
 </script>
 
-<style>
-
-</style>
