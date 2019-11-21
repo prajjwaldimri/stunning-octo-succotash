a488b6a24552a9c364cfecca3cd6208f3f588264
gauravuniyal1998 <gauravuniyal1998@gmail.com>
2019-11-21 19:16:20 +0530

    Removed the S.O.S.ial Logo from Home.Vue

--- client/src/userPages/Home.vue
+++ client/src/userPages/Home.vue
@@ -1,21 +1,53 @@
 <template>
 <div>
      <div class="container">
-       <img src="../assets/logo.png" alt="" width="140" height="100">
         <app-header></app-header>
-        <hr>
+        <hr><!--Horizontal Line after the Header> <!-->
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+
         <app-footer></app-footer>
     </div>
 </div>
-  
-    
+
+
 </template>
 
 <script>
     import Header from '../components/Header';
     import Footer from '../components/Footer';
 export default {
-  
+
   components: {
     'app-header': Header,
     'app-footer': Footer
