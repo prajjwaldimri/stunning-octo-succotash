8efc0ca668423f9fab32bff3a8052fe4f3221916
kaisharma04 <krishanu.sharma619@gmail.com>
2019-11-21 15:27:00 +0530

    importing vueRouter in main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
 import Home from './userPages/Home.vue';
+
+Vue.use(VueRouter);
 
 Vue.component('homepage-status', Home);
 
