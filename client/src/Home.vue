06d598a33cace1522c77de34a89905b33ecbd795
kaisharma04 <krishanu.sharma619@gmail.com>
2019-11-22 00:01:18 +0530

    made changes to home.vue

---
+++ client/src/Home.vue
@@ -0,0 +1,61 @@
+<template>
+<div>
+     <div class="container">
+        <app-header></app-header>
+        <hr><!--Horizontal Line after the Header> <!-->
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+        <p> asjkdasjkhsdfjsdfdsfsdklfjskldfjsdklfjsdlfjsdklfjsdlfsd</p>
+
+        <app-footer></app-footer>
+    </div>
+</div>
+
+
+</template>
+
+<script>
+    import Header from '../components/Header';
+    import Footer from '../components/Footer';
+export default {
+
+  components: {
+    'app-header': Header,
+    'app-footer': Footer
+
+  }
+}
+</script>
+
+<style >
+
+</style>
