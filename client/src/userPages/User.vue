6459aef581b13b451adb2b3804443a252be83a8f
rhdew <rdewari07@gmail.com>
2019-12-08 15:22:17 +0530

    removed merge conflict

--- client/src/userPages/User.vue
+++ client/src/userPages/User.vue
@@ -1,4 +1,31 @@
 <template>
-    <h1>The user page</h1>
-
+<div class="container">
+    <app-header></app-header>
+    <app-newPost></app-newPost>
+    <app-feedPost></app-feedPost>
+    <app-feedPost></app-feedPost>
+    <app-footer></app-footer>
+        
+</div>
 </template>
+
+<script>
+import Header from '../components/Header';
+import NewPost from '../components/NewPost';
+import FeedPost from '../components/FeedPost';
+import Footer from '../components/Footer';
+
+export default {
+
+  components: {
+    'app-header': Header,
+    'app-newPost':NewPost,
+    'app-feedPost':FeedPost,
+    'app-footer': Footer
+
+  },
+
+}
+</script>
+<style scoped>
+</style>
